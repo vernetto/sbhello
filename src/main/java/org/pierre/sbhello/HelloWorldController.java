424126e06eacc1133699e1c44de3dc3c0de24b56
vernetto <publicpierre@gmail.com>
2019-08-10 09:13:24 +0200

    hello

---
+++ src/main/java/org/pierre/sbhello/HelloWorldController.java
@@ -0,0 +1,15 @@
+package org.pierre.sbhello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @RequestMapping("world")
+    String helloWorld() {
+        return "Hello World - v3!";
+    }
+
+}
---
+++ src/main/java/org/pierre/sbhello/SbhelloApplication.java
@@ -0,0 +1,13 @@
+package org.pierre.sbhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbhelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbhelloApplication.class, args);
+    }
+
+}
